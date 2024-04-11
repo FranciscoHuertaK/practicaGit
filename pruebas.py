34355008dae41cb151c316dcd52e2f4b0b7e7bbf
Francisco Huerta Kretsch <francisco.huertak@uanl.edu.mx>
2024-04-11 13:06:20 -0600

    Se añade la prueba para la funcion del area del cuadrado

--- pruebas.py
+++ pruebas.py
@@ -3,3 +3,7 @@ import ejercicio1
 #Probar la tabla
 numero = int(input("Que tabla quieres?"))
 ejercicio1.tabla(numero)
+
+#Probar la funcion de area del cuadrado
+lado = int(input("Dame la longitud de un lado del cuadrado: "))
+ejercicio1.area_cuadrado(lado)
