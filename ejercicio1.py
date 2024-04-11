ebd430764936efb7dba75c32c0f966d50f94665f
Francisco Huerta Kretsch <francisco.huertak@uanl.edu.mx>
2024-04-11 13:01:33 -0600

    Se crea la funcion para realizar operaciones

--- ejercicio1.py
+++ ejercicio1.py
@@ -5,3 +5,13 @@ def tabla(numero):
 def area_cuadrado(lado):
     print(f"El area es: {lado*lado}")
 
+def operaciones(numero1,numero2,operacion):
+    if operacion == 1:
+        print(f"La suma de {numero1} mas {numero2} es igual a {numero1+numero2}.")
+    elif operacion == 2:
+        print(f"La resta de {numero1} menos {numero2} es igual a {numero1-numero2}.")
+    elif operacion == 3:
+        print(f"La multiplicacion de {numero1} por {numero2} es igual a {numero1*numero2}.")
+    elif operacion == 4:
+        print(f"La division de {numero1} entre {numero2} es igual a {numero1/numero2}.")
+
